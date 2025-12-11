80c99cdc8ba9105cdebad4d4e312e2dd8a2bca91
Amir Kooshky <kooshkya@gmail.com>
2025-12-11 10:02:35 +0330

    add error checking to deploy_knbill

--- scripts/setup/deploy_knbill.sh
+++ scripts/setup/deploy_knbill.sh
@@ -26,32 +26,35 @@
 MASTER_NODE=$1
 
 server_exec() { 
-	ssh -oStrictHostKeyChecking=no -p 22 $MASTER_NODE $1;
+    ssh -oStrictHostKeyChecking=no -p 22 $MASTER_NODE "$1"
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Command failed: $1"
+        exit 1
+    fi
 }
 
 {
-	echo 'Setting up KnBill components'
-	
- 	server_exec \
-        "kubectl patch configmap -n knative-serving config-observability \
+    echo 'Setting up KnBill components'
+    
+    server_exec "kubectl patch configmap -n knative-serving config-observability \
         --type='merge' \
         -p '{\"data\": {\"logging.enable-request-log\": \"true\"}}'"
 
-    server_exec \
-        "kubectl patch configmap -n knative-serving config-observability \
+    server_exec "kubectl patch configmap -n knative-serving config-observability \
         --type='merge' \
         -p '{\"data\": {\"logging.request-log-template\": \"{\\\"httpRequest\\\": {\\\"requestMethod\\\": \\\"{{.Request.Method}}\\\", \\\"requestUrl\\\": \\\"{{js .Request.RequestURI}}\\\", \\\"requestSize\\\": \\\"{{.Request.ContentLength}}\\\", \\\"status\\\": {{.Response.Code}}, \\\"responseSize\\\": \\\"{{.Response.Size}}\\\", \\\"userAgent\\\": \\\"{{js .Request.UserAgent}}\\\", \\\"remoteIp\\\": \\\"{{js .Request.RemoteAddr}}\\\", \\\"serverIp\\\": \\\"{{.Revision.PodIP}}\\\", \\\"referer\\\": \\\"{{js .Request.Referer}}\\\", \\\"latency\\\": \\\"{{.Response.Latency}}s\\\", \\\"protocol\\\": \\\"{{.Request.Proto}}\\\"}, \\\"traceId\\\": \\\"{{index .Request.Header \\\"X-B3-Traceid\\\"}}\\\"}\"}}'"
 
+    # kubectl create/apply with error check
     server_exec "kubectl create namespace logging"
     server_exec "kubectl create -f https://raw.githubusercontent.com/fluent/fluent-bit-kubernetes-logging/master/fluent-bit-service-account.yaml"
     server_exec "kubectl create -f https://raw.githubusercontent.com/fluent/fluent-bit-kubernetes-logging/master/fluent-bit-role.yaml"
     server_exec "kubectl create -f https://raw.githubusercontent.com/fluent/fluent-bit-kubernetes-logging/master/fluent-bit-role-binding.yaml"
-
     server_exec "kubectl apply -f https://raw.githubusercontent.com/kooshkya/invitro/refs/heads/kooshkya/knbill/config/fluent-bit-collector.yaml"
     server_exec "kubectl apply -f https://raw.githubusercontent.com/kooshkya/invitro/refs/heads/kooshkya/knbill/config/fluent-bit-configmap.yaml"
     server_exec "kubectl apply -f https://raw.githubusercontent.com/kooshkya/invitro/refs/heads/kooshkya/knbill/config/fluent-bit-ds.yaml"
 
-	echo 'Done setting up KnBill components'
+    echo 'Done setting up KnBill components'
 
-	exit
+    exit
 }
+
